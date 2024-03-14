31de7f03d7326dcc61d38f718172d16e68eb3ef2
szzin20 <92289030+szzin20@users.noreply.github.com>
2024-03-14 11:44:06 +0700

    feat: add mvvm

---
+++ praktikum_03/lib/model/user.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class User {
+  final String name;
+  final int number;
+
+  User({
+    required this.name,
+    required this.number,
+  });
+
+}
---
+++ praktikum_03/lib/view_model/contact_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import '../../model/user.dart';
+part 'contact_state.dart';
+
+class ContactCubit extends Cubit<ContactState> {
+  ContactCubit() : super(ContactInitial());
+
+  List<User> user = [];
+
+  void addContact({required String username, required int number}) async {
+    emit(ContactInitial());
+    user.add(User(name: username, number: number));
+    emit(ContactLoading());
+    await Future.delayed(
+        const Duration(seconds: 2), () => emit(ContactLoaded(user: user)));
+  }
+
+  void removeUser({required int index}) {
+    emit(ContactInitial());
+    user.removeAt(index);
+    if (user.isEmpty) {
+      emit(ContactInitial());
+    } else {
+      emit(ContactLoaded(user: user));
+    }
+  }
+}
