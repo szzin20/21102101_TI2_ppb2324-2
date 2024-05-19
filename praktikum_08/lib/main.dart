551578ad0b8e371c8c970ae54bc60cb3ab717ff7
szzin20 <92289030+szzin20@users.noreply.github.com>
2024-05-19 22:21:52 +0700

    feat: add firebase google auth

--- praktikum_08/lib/main.dart
+++ praktikum_08/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:praktikum_08/ui/home_screen.dart';
+import 'package:praktikum_08/ui/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:praktikum_08/bloc/login/login_cubit.dart';
+import 'bloc/register/register_cubit.dart';
 import 'package:praktikum_08/ui/splash.dart';
 import 'package:praktikum_08/utils/routes.dart';
 import 'firebase_options.dart';
@@ -21,14 +25,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider(create: (context) => LoginCubit()), // Provide the LoginCubit
+        BlocProvider(create: (context) => LoginCubit()),
+        BlocProvider(create: (context) => RegisterCubit())
       ],
       child: MaterialApp(
-        title: "Praktikum 8", // Update the title to "Praktikum 8"
+        title: "Praktikum 8",
         debugShowCheckedModeBanner: false,
         navigatorKey: NAV_KEY,
         onGenerateRoute: generateRoute,
-        home: const SplashScreen(),
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasData) {
+              return const HomeScreen();
+            } else if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+            } else {
+              return const LoginScreen();
+            }
+          },
+        ),
       ),
     );
   }
--- praktikum_08/lib/ui/home_screen.dart
+++ praktikum_08/lib/ui/home_screen.dart
@@ -1,19 +1,39 @@
+import 'package:praktikum_08/ui/login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          "HOME SCREEN",
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Color(0xff3D4DE0),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "HOME SCREEN",
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff3D4DE0)),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                        (route) => false));
+              },
+              child: const Text('Keluar'),
+            ),
+          ],
         ),
       ),
     );
